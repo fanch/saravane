ba711a64c34b3fe8c1ae59afb4bef0306d7ebd71
tnut <tnut at nutyx dot org>
2015-02-05 20:55:17 +0100

    maj version script blfs et documentation.html

--- basicfs/rootfs/etc/profile.d/qt4.sh
+++
@@ -1,3 +0,0 @@
-pathappend /usr/bin                   PATH
-pathappend /usr/lib/pkgconfig/        PKG_CONFIG_PATH
-
--- basicfs/rootfs/etc/profile.d/qt5.sh
+++
@@ -1,2 +0,0 @@
-pathappend /usr/lib/qt5/bin           PATH
-pathappend /usr/lib/pkgconfig/        PKG_CONFIG_PATH
